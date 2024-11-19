2a1d0918f6b49952ccd0b4ad04c59803dd254d9c
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-18 18:19:14 -0800

    debugging using cy pause()

--- cypress/e2e/click.cy.ts
+++ cypress/e2e/click.cy.ts
@@ -2,7 +2,14 @@ describe('CLICK', () => {
     beforeEach(() => {
         cy.visit('/click')
     });
-    it('debug click', () => {
+    it.skip('debug click', () => {
         cy.get('#badButton').debug().click();
     });
+
+    it('click with pause', () => {
+        cy.pause()
+        cy.get('#badButton').click();
+        cy.pause()
+    });
+
 });
