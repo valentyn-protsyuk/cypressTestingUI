232e0bf4d7c1a961a172b98e935a5d8e35406cef
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-18 20:10:12 -0800

    moved autofill part to a different repo

--- cypress/e2e/autoFill.cy.ts
+++
@@ -1,9 +0,0 @@
-import {AutoFillPage} from "../../pages/AutoFill"
-describe('AUTO FILL', () => {
-    beforeEach(() => {
-        cy.visit('/auto-complete')
-    });
-    it('', () => {
-        
-    });
-});
