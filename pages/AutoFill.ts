2cb592fe62657b48d479ccd782eeb7b442162f9d
valentyn_protsyuk <valentyn.protsyuk@yahoo.com>
2024-11-18 19:59:29 -0800

    autofill class function done

--- pages/AutoFill.ts
+++ pages/AutoFill.ts
@@ -1,5 +1,17 @@
 class AutoFill {
+    private inputField: string = '.auto-complete__value-container'
+    private dropDown: string = '#react-select-2-option-0'
+    private result: string = '.auto-complete__multi-value__label'
 
+    autoComplete(){
+        const obj = {
+            Y: 'Yellow'
+        }
+
+        cy.get(this.inputField).eq(0).type(Object.keys(obj)[0]) //Type: Y
+        cy.contains(this.dropDown,Object.values(obj)[0]).click() //click on 'Yellow' in drop down
+        cy.get(this.result).should('have.text', Object.values(obj)[0]) //check that label = yellow
+    }
 }
 
 export const AutoFillPage = new AutoFill();
